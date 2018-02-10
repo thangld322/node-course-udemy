791e8498cb2fa66278eb9da83fe1c2c2f68ff6ee
Le Thang <thangld322@gmail.com>
2018-02-10 23:35:29 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('unable to connect to server');
+    }
+  });
+  next();
+});
+
+app.use((req,res,next) => {
+  res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello express!</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Chao` con ga`'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'badbit'
+  });
+})
+
+app.listen(3000);
